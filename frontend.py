8b842a337b387aaf261d74425b2989c404b8a1ec
Archit Goswami <ajg6278@psu.edu>
2022-06-07 20:25:50 -0700

    Changes to frontend

--- frontend.py
+++ frontend.py
@@ -1,11 +1,47 @@
+import tkinter as tk
 from tkinter import *
+
+mywindow = tk.Tk()
+
+mywindow.title("Python Tkinter")
+mywindow.minsize(500,400)
+
+def buttonPress():
+    print("Button Pressed!!")
+
+def textBox():
+    print(textb.get())
+
+def printInput():
+    inp = textb.get(1.0, "end-1c")
+    lbl.config(text = "Provided Input: "+inp)
+
+#Label
+label = tk.Label(mywindow, text="Label Text: ")
+label.place(relx=0.4, rely=0.3, anchor=CENTER)
+
+
+a = Button(text="Center Button")
+a.place(relx=0.58, rely=0.3, anchor=CENTER)
+
  
-class Root(Tk):
-    def __init__(self):
-        super(Root,self).__init__()
- 
-        self.title("Python Tkinter")
-        self.minsize(500,400)
- 
-root = Root()
-root.mainloop()
+#Textbox
+textb = tk.Entry(mywindow,text="Entry")
+textbutton = tk.Button(mywindow, text="Text Box", command = printInput)
+textbutton.place(relx=0.5, rely=0.7, anchor=CENTER)
+
+message ='Hi'
+
+text_box = Text(
+    height=5,
+    width=20
+)
+text_box.pack(expand=True)
+text_box.insert('end', message)
+text_box.config(state='disabled')
+
+lbl = tk.Label(mywindow, text = "")
+lbl.pack()
+
+mywindow.mainloop()
+
